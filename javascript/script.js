d5921e08ae903a6b560f65cdc903b9f45304f94c
Dita Mukane <dita.mukane@gmail.com>
2023-03-09 09:17:59 +0100

    last comit

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,7 @@ document.querySelector("#i1").addEventListener('click', function() {
     Swal.fire({
         title: 'Egg Salad',
         html: '6 cups spring mix salad greens <br> 2 medium tomatoes, coarsely chopped <br> 6 hard-boiled large eggs, coarsely chopped <br> 3 slices deli turkey, cut into thin strips <br> 3 slices deli ham, cut into thin strips <br> 1/2 cup shredded cabbage <br> 4 green onions, sliced <br> 4 fresh baby carrots, sliced <br> 4 radishes, thinly sliced <br> 1/4 teaspoon garlic powder <br> 1/4 teaspoon pepper <br> 1/2 cup reduced-fat Thousand Island salad dressing or dressing of your choice  <br> <br>INSTRUCTIONS <br> <br>In a large bowl, combine the first 9 ingredients. Sprinkle with garlic powder and pepper; toss to coat. Serve with salad dressing.',
-        imageUrl: '/images/EggsSalad.jpg',
+        imageUrl: 'images/EggsSalad.jpg',
         imageWidth: 400,
         // imageHeight: 200,
         imageAlt: 'Custom image',
@@ -13,7 +13,7 @@ document.querySelector("#i2").addEventListener('click', function() {
     Swal.fire({
         title: 'Cheeseburger',
         html: 'INGREDIENTS <br>  <br>½ cup mayonnaise <br> ¼ cup ketchup <br> 3 tablespoons dill pickle relish  <br> 1 tablespoon Dijon mustard <br> 2 pounds ground beef, 80/20 <br> Kosher salt and freshly ground black pepper, to taste  <br> 1 tablespoon canola oil  <br> 6 slices American cheese <br>  <br> FOR SERVING  <br>  <br>Brioche hamburger buns, homemade or store-bought  <br>Romaine or shredded lettuce <br>Sliced tomato <br>Sliced red onion <br>Dill pickle chips <br> <br>INSTRUCTIONS <br> <br>BURGER SAUCE: In a small bowl, whisk together mayonnaise, ketchup, dill pickle relish and Dijon; set aside. <br> <br>In a large bowl, combine beef, 1 1/2 teaspoons salt and 1 1/2 teaspoons pepper. Using a wooden spoon or clean hands, stir until well combined. Gently form into 6 1-inch-thick patties, about the size of the hamburger buns. <br> <br>Heat canola oil in a large cast iron skillet over medium high heat. Add patties and cook until lightly charred or until desired doneness, about 3-5 minutes per side; top with cheese. <br> <br>Serve immediately in hamburger buns with BURGER SAUCE and desired toppings.',
-        imageUrl: '/images/Cheesburger.jpg',
+        imageUrl: 'images/Cheesburger.jpg',
         imageWidth: 400,
         // imageHeight: 200,
         imageAlt: 'Custom image',
@@ -24,7 +24,7 @@ document.querySelector("#i3").addEventListener('click', function() {
     Swal.fire({
         title: 'Basil Pizza',
         html: 'INGREDIENTS <br>  <br>2 1/4 to 2 1/2 pounds (about 1.25kg) small-to-medium sized fresh tomatoes, sliced in half <br>3 cloves fresh garlic, peeled and quartered lengthwise or thickly sliced <br>2 tablespoons olive oil, plus additional oil for the pizza <br>12 branches of fresh thyme, or 1 teaspoon dried thyme <br>1 bay leaf <br>sea salt and freshly ground black pepper <br>12 ounces fresh , pizza dough <br>8 ounces (240g) mozzarella cheese, sliced <br>One bunch of fresh basil <br>Parmesan cheese <br> <br>INSTRUCTIONS <br> <br>Preheat the oven to 350ºF (180ºC).<br> <br>On a non-stick baking sheet (or one lined with parchment paper) that’s large enough to hold the tomatoes in a single layer, toss together the tomatoes with the garlic, olive oil, thyme, bay leaf, and season with salt and pepper. (I usually finds it takes a bit more salt than I think, but you can add more salt after roasting.)<br> <br>Roast in the oven for 1 hour, checking midway during roasting to make sure the garlic isn’t burning. If the tomatoes are getting overly dark on the bottom, turn them gently with a spatula.<br> <br>After one hour, turn off oven and leave tomatoes in the oven until oven cools completely. Roasting time will vary depending on the size of tomatoes; just roast them until they’ve given up their juices, as shown in the picture in the post. (Tomatoes can be roasted up to three days in advance and stored in the refrigerator. They can also be frozen in freezer bags for several months.)<br> <br>To make the pizza, preheat the oven to 450ºF (235ºC) and place a heavy baking sheet on rack on the lower third of the oven.<br> <br>On a lightly floured surface, roll and stretch the pizza dough into a 14-inch (36 cm) circle. Place the dough on a sheet of parchment paper.<br> <br>Brush the dough with a thin layer of olive oil. Drain the tomatoes well (reserve the liquid for a vinaigrette or soup), then distribute the tomatoes and garlic over the dough. Put the mozzarella slices over the tomatoes, then place a dozen fresh basil leaves on top of the pizza and grate some Parmesan over the top.<br> <br> Return the pizza to the oven and bake for 10 to 20 minutes (see Note), until the pizza is baked to your liking\\; the bottom crust should be crisp and the top should be slightly browned and bubbling.<br> <br>Remove from the oven and strew chopped fresh basil over the top along with a generous sprinkling of additional grated Parmesan cheese.',
-        imageUrl: '/images/BasilPizza.jpg',
+        imageUrl: 'images/BasilPizza.jpg',
         imageWidth: 400,
         // imageHeight: 200,
         imageAlt: 'Custom image',
